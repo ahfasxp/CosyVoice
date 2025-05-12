7bccf0dc9c1a9843f7287d79765542eb74152616
ahfasxp <ahfas.fazria@gmail.com>
2025-05-12 08:26:37 +0700

    feat: Add downloading functionality for CosyVoice-300M-SFT model

--- download_models.py
+++ download_models.py
@@ -8,4 +8,8 @@ print("Downloading CosyVoice-ttsfrd model...")
 snapshot_download('iic/CosyVoice-ttsfrd', local_dir='pretrained_models/CosyVoice-ttsfrd')
 print("Finished downloading CosyVoice-ttsfrd model.")
 
+print("Downloading CosyVoice-300M-SFT model...")
+snapshot_download('iic/CosyVoice-300M-SFT', local_dir='pretrained_models/CosyVoice-300M-SFT')
+print("Finished downloading CosyVoice-300M-SFT model.")
+
 print("All models downloaded.")
